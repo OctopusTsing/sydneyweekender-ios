898312ae2b281125461872161bcf84f397d6985b
kinyloin <1429751374@qq.com>
2026-05-17 09:55:19 +1000

    Initial Commit

---
+++ sydweekender-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sydweekender-ios
+//
+//  Created by 文宇 on 17/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sydweekender-ios/sydweekender_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sydweekender_iosApp.swift
+//  sydweekender-ios
+//
+//  Created by 文宇 on 17/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct sydweekender_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
